26bd2c45d82ade07b516e6d1c6c8964aa3df4a11
Ziqi Zhang <a1909438@adelaide.edu.au>
2025-03-27 09:02:07 +1030

    feat: constructed the HTTP GET request

--- Proxy.py
+++ Proxy.py
@@ -148,6 +148,8 @@ while True:
       # originServerRequest is the first line in the request and
       # originServerRequestHeader is the second line in the request
       # ~~~~ INSERT CODE ~~~~
+      originServerRequest = "GET " + resource + " HTTP/1.1"
+      originServerRequestHeader = "Host: " + hostname
       # ~~~~ END CODE INSERT ~~~~
 
       # Construct the request to send to the origin server
