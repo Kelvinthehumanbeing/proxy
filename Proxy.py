68b0c365dcf4cc35eba96741628b14a03a591e77
Ziqi Zhang <a1909438@adelaide.edu.au>
2025-03-27 09:26:49 +1030

    feat: send cached response to client on cache hit

--- Proxy.py
+++ Proxy.py
@@ -118,6 +118,8 @@ while True:
     # ProxyServer finds a cache hit
     # Send back response to client 
     # ~~~~ INSERT CODE ~~~~
+    response = ''.join(cacheData)
+    clientSocket.sendall(response.encode())
     # ~~~~ END CODE INSERT ~~~~
     cacheFile.close()
     print ('Sent to the client:')
